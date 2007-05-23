94627b236307e906c442b5d139af727e6783db25
ryanjoneil <ryanjoneil@10f34b66-a92c-0410-b755-39e9f4ab4909>
2007-05-23 04:08:02 +0000

    better population tests

--- pygep/src/tests/population.py
+++ pygep/src/tests/population.py
@@ -17,10 +17,28 @@ class SillyComputation(Computation):
             return 0
 
 
+class ZeroFitnessComputation(SillyComputation):
+    def _fitness(self):
+        return 0
+    
+    def _solved(self):
+        return True
+
+
 class PopulationTest(unittest.TestCase):
     '''Tests population cycling'''
     def setUp(self):
         self.pop = Population(SillyComputation, 10, 5, 1)
+        
+        # Turn all variation on all the time
+        self.pop.mutation_rate            = 1.1 # additional .1 = sanity check
+        self.pop.inversion_rate           = 1.1
+        self.pop.is_transposition_rate    = 1.1
+        self.pop.ris_transposition_rate   = 1.1
+        self.pop.gene_transposition_rate  = 1.1
+        self.pop.crossover_one_point_rate = 1.1
+        self.pop.crossover_two_point_rate = 1.1
+        self.pop.crossover_gene_rate      = 1.1
     
     
     def testBestFitness(self):
@@ -30,10 +48,37 @@ class PopulationTest(unittest.TestCase):
     
     def testCycle(self):
         self.assertEqual(self.pop.age, 0)
-        self.pop.cycle()
+        first_best = self.pop.best
+        
+        self.pop.solve(1)
         self.assertEqual(self.pop.age, 1)
         self.assertEqual(len(self.pop), 10)
+        self.assertTrue(self.pop[0] is first_best)
+        
+        
+    def testZeroFitness(self):
+        # Special case: mean fitness <= 0
+        p = Population(ZeroFitnessComputation, 10, 5, 1)
+        p.cycle()
+        self.assertEqual(0, p.mean)
+        self.assertEqual(0, p.stdev)
+        
+
+    def testStopOnSolve(self):
+        p = Population(ZeroFitnessComputation, 10, 5, 1)
+        p.solve(100)
+        self.assertEqual(0, p.age)
     
+    
+    def testEmptyPopulation(self):
+        self.assertRaises(ValueError, Population, SillyComputation, 0, 5, 1)
+    
+    
+    def testPopulationRepr(self):
+        p = repr(self.pop)
+        for c in self.pop:
+            self.assertTrue(repr(c) in p)
+
 
 if __name__ == '__main__':
     unittest.main()
